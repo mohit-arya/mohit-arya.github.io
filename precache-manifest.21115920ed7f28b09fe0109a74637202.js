c16e30db53ec5cb5e62021961c3eb1e4dd6a4e49
mohit-arya <mohitarya@outlook.com>
2019-10-13 22:55:21 +0530

    init

---
+++ precache-manifest.21115920ed7f28b09fe0109a74637202.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc08eac4f733ce7efaf90e247c8fef64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "945a7dd817cf52a50485",
+    "url": "/static/css/main.fabbef7f.chunk.css"
+  },
+  {
+    "revision": "9ac5cd3a149e1313e847",
+    "url": "/static/js/2.424ca91e.chunk.js"
+  },
+  {
+    "revision": "945a7dd817cf52a50485",
+    "url": "/static/js/main.38a4cc72.chunk.js"
+  },
+  {
+    "revision": "4d2eb8994f12ecbf5885",
+    "url": "/static/js/runtime-main.31c55287.js"
+  }
+]);
